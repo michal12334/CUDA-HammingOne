094248924bde2ed83394e5eff074f8df31659fda
Michal Boguszewski <michal1842001@gmail.com>
2022-11-13 12:42:31 +0100

    Create cpu version

--- cpuVersion/main.cpp
+++ cpuVersion/main.cpp
@@ -2,7 +2,39 @@
 
 using namespace std;
 
+int hammingDistance(char* a, char* b, int l) {
+    int result = 0;
+    for(int i = 0; i < l; i++) {
+        if(a[i] != b[i])
+            result++;
+    }
+    return result;
+}
+
 int main() {
-    cout << "Hello world!" << endl;
+    int n, l;
+    char** numbers;
+    cin >> n >> l;
+    numbers = new char*[n];
+    for(int i = 0; i < n; i++) {
+        numbers[i] = new char[l + 1];
+    }
+
+    for(int i = 0; i < n; i++) {
+        cin >> numbers[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(hammingDistance(numbers[i], numbers[j], l) == 1) {
+                cout << i << " " << j << "\n";
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        delete[] numbers[i];
+    }
+    delete[] numbers;
     return 0;
 }
