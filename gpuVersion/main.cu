3a7712a1b2542baff03d732a5c3c1003c9250ef1
Michal Boguszewski <michal1842001@gmail.com>
2022-11-15 22:07:44 +0100

    Refactor code

--- gpuVersion/main.cu
+++ gpuVersion/main.cu
@@ -19,12 +19,13 @@ __device__ Pair hammingDistance(char* a, char* b, int l) {
     return result;
 }
 
-__global__ void compute(char* d_mem, int n, int l, bool* d_isAnswer) {
+__global__ void compute(char* d_mem, int n, int l, int* d_pairs, int* d_numberOfPairs) {
     int tid = threadIdx.x + blockIdx.x;
     for(int i = tid + 1; i < n; i++) {
         auto hd = hammingDistance(d_mem + tid*l, d_mem + i*l, l);
         if(hd.x == 1) {
-            d_isAnswer[tid * l + hd.y] = true;
+            d_pairs[d_numberOfPairs[tid] + tid*l] = i;
+            d_numberOfPairs[tid]++;
         }
     }
 }
@@ -35,36 +36,32 @@ int main() {
     const int NUMBER_OF_THREADS = 1024;
     const int NUMBER_OF_BLOCKS = n / 1024 + 1;
     char* mem = new char[n * l + 1];
-    bool* isAnswer = new bool[n * l];
+    int* pairs = new int[n * l];
+    int* numberOfPairs = new int[n];
     for(int i = 0; i < n; i++)
         std::cin >> (mem + i * l);
     char* d_mem;
-    bool* d_isAnswer;
+    int* d_pairs;
+    int* d_numberOfPairs;
     cudaMalloc(&d_mem, n * l * sizeof(char));
-    cudaMalloc(&d_isAnswer, n * l * sizeof(bool));
+    cudaMalloc(&d_pairs, n * l * sizeof(int));
+    cudaMalloc(&d_numberOfPairs, n * sizeof(int));
     cudaMemcpy(d_mem, mem, n * l * sizeof(char), cudaMemcpyHostToDevice);
-    cudaMemset(d_isAnswer, 0, n * l * sizeof(bool));
+    cudaMemset(d_numberOfPairs, 0, n * sizeof(int));
 
-    compute<<<NUMBER_OF_BLOCKS, NUMBER_OF_THREADS>>>(d_mem, n, l, d_isAnswer);
+    compute<<<NUMBER_OF_BLOCKS, NUMBER_OF_THREADS>>>(d_mem, n, l, d_pairs, d_numberOfPairs);
 
-    cudaMemcpy(isAnswer, d_isAnswer, n * l * sizeof(bool), cudaMemcpyDeviceToHost);
+    cudaMemcpy(pairs, d_pairs, n * l * sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(numberOfPairs, d_numberOfPairs, n* sizeof(int), cudaMemcpyDeviceToHost);
 
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < l; j++) {
-            if(isAnswer[i * l + j]) {
-                for(int k = 0; k < l; k++) {
-                    std::cout << mem[i*l + k];
-                }
-                std::cout << " ";
-                for(int k = 0; k < l; k++) {
-                    if(k == j) {
-                        std::cout << (mem[i*l + k] == '1' ? '0' : '1');
-                        continue;
-                    }
-                    std::cout << mem[i*l + k];
-                }
-                std::cout << "\n";
-            }
+        for(int j = 0; j < numberOfPairs[i]; j++) {
+            for(int k = 0; k < l; k++)
+                std::cout << mem[k + i*l];
+            std::cout << " ";
+            for(int k = 0; k < l; k++)
+                std::cout << mem[k + pairs[j + i*l]*l];
+            std::cout << "\n";
         }
     }
 
