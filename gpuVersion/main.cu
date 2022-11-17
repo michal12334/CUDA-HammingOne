e940917ccbffb86a9256a63668e2bec02b228d84
Michal Boguszewski <michal1842001@gmail.com>
2022-11-17 19:26:02 +0100

    Try to use shared memory

--- gpuVersion/main.cu
+++ gpuVersion/main.cu
@@ -19,14 +19,42 @@ __device__ Pair hammingDistance(char* a, char* b, int l) {
     return result;
 }
 
-__global__ void compute(char* d_mem, int n, int l, int* d_pairs, int* d_numberOfPairs) {
-    int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    for(int i = tid + 1; i < n; i++) {
-        auto hd = hammingDistance(d_mem + tid*l, d_mem + i*l, l);
-        if(hd.x == 1) {
-            d_pairs[d_numberOfPairs[tid] + tid*l] = i;
-            d_numberOfPairs[tid]++;
+__global__ void compute(char* d_mem, int n, int l, bool* d_pairs) {
+    extern __shared__ char shm[];
+    int mem1Offset = 32*l;
+
+    int tidX = threadIdx.x;
+    int tidY = threadIdx.y;
+    int tid = tidX + tidY * 32;
+    int bidX = blockIdx.x;
+    int bidY = blockIdx.y;
+
+    if(tidX >= n || tidY >= n)
+        return;
+    
+    int a = bidX * 32 * n * l;
+    int b = bidY * 32 * n * l;
+    for(int i = tid; i < mem1Offset; i+= blockDim.x*blockDim.y) {
+        int index = i;
+        shm[index] = d_mem[a + index/32 + (i%32)*l];
+        shm[index + mem1Offset] = d_mem[b + index/32 + (i%32)*l];
+    }
+
+    __syncthreads();
+
+    if(tidX >= tidY)
+        return;
+
+    Pair hm;
+    hm.x = 0;
+    for(int i = 0; i < l; i++) {
+        if(shm[tidX + 32*i] != shm[tidY + 32*i + mem1Offset]) {
+            hm.x++;
+            hm.y = i;
         }
+    }
+    if(hm.x == 1) {
+        d_pairs[a + tidX*l + hm.y] = true;
     }
 }
 
@@ -36,32 +64,35 @@ int main() {
     const int NUMBER_OF_THREADS = 1024;
     const int NUMBER_OF_BLOCKS = n / 1024 + 1;
     char* mem = new char[n * l + 1];
-    int* pairs = new int[n * l];
-    int* numberOfPairs = new int[n];
+    bool* pairs = new bool[n * l];
     for(int i = 0; i < n; i++)
         std::cin >> (mem + i * l);
     char* d_mem;
-    int* d_pairs;
-    int* d_numberOfPairs;
+    bool* d_pairs;
     cudaMalloc(&d_mem, n * l * sizeof(char));
-    cudaMalloc(&d_pairs, n * l * sizeof(int));
-    cudaMalloc(&d_numberOfPairs, n * sizeof(int));
+    cudaMalloc(&d_pairs, n * l * sizeof(bool));
     cudaMemcpy(d_mem, mem, n * l * sizeof(char), cudaMemcpyHostToDevice);
-    cudaMemset(d_numberOfPairs, 0, n * sizeof(int));
+    cudaMemset(d_pairs, 0, n * l * sizeof(bool));
 
-    compute<<<NUMBER_OF_BLOCKS, NUMBER_OF_THREADS>>>(d_mem, n, l, d_pairs, d_numberOfPairs);
+    size_t shmSize = (64*l) * sizeof(char);
+    // size_t shmSize = 1e9;
+    compute<<<dim3(sqrt(n)/32 + 1, sqrt(n)/32 + 1, 1), dim3(32, 32, 1), shmSize>>>(d_mem, n, l, d_pairs);
 
-    cudaMemcpy(pairs, d_pairs, n * l * sizeof(int), cudaMemcpyDeviceToHost);
-    cudaMemcpy(numberOfPairs, d_numberOfPairs, n* sizeof(int), cudaMemcpyDeviceToHost);
+    cudaMemcpy(pairs, d_pairs, n * l * sizeof(bool), cudaMemcpyDeviceToHost);
 
     for(int i = 0; i < n; i++) {
-        for(int j = 0; j < numberOfPairs[i]; j++) {
-            for(int k = 0; k < l; k++)
-                std::cout << mem[k + i*l];
-            std::cout << " ";
-            for(int k = 0; k < l; k++)
-                std::cout << mem[k + pairs[j + i*l]*l];
-            std::cout << "\n";
+        for(int j = 0; j < l; j++) {
+            if(pairs[j + i*l]) {
+                for(int k = 0; k < l; k++)
+                    std::cout << mem[k + i*l];
+                std::cout << " ";
+                for(int k = 0; k < l; k++)
+                    if(k == j)
+                        std::cout << (mem[k + i*l] == '0' ? '1' : '0');
+                    else
+                        std::cout << mem[k + i*l];
+                std::cout << "\n";
+            }
         }
     }
 
