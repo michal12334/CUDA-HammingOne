6a7a49e719644cf10f4fae3c5b093c592eaf9bb6
Michal Boguszewski <michal1842001@gmail.com>
2022-12-03 14:06:19 +0100

    Refactot test

--- test/test.sh
+++ test/test.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+func() {
+    echo TEST$1
+    ./inputGenerator $2 $3 $4 > in/in$1.in
+    ./cpu < in/in$1.in > out/cpuOut$1.out
+    ./gpu < in/in$1.in > out/gpuOut$1.out
+    diff out/cpuOut$1.out out/gpuOut$1.out
+}
+
 g++ -o cpu ../cpuVersion/main.cpp
 nvcc -o gpu ../gpuVersion/main.cu
 g++ -o inputGenerator ../inputGenerator/main.cpp
@@ -8,12 +16,21 @@ mkdir -p in out
 
 for i in {1..1000}
 do
-    echo TEST$i
-    ./inputGenerator 10 1000 4 > in/in$i.in
-    ./cpu < in/in$i.in > out/cpuOut$i.out
-    ./gpu < in/in$i.in > out/gpuOut$i.out
-    diff out/cpuOut$i.out out/gpuOut$i.out
+    func $i 10 10 4
 done
+
+for i in {1001..1500}
+do
+    func $i 10 1000 5
+done
+
+for i in {1501..2000}
+do
+    func $i 100 1000 60
+done
+
+
+# func 2001 10000 1000 2000
 
 rm cpu
 rm gpu
