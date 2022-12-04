a8c50e102968c4dec24fdd06ea2d4aa3f4bb0369
Michal Boguszewski <michal1842001@gmail.com>
2022-12-04 21:31:45 +0100

    Improve cpu version

--- cpuVersion/main.cpp
+++ cpuVersion/main.cpp
@@ -1,40 +1,47 @@
 #include <iostream>
 
+#define WORD_MAX_SIZE 32
+
 using namespace std;
 
-int hammingDistance(char* a, char* b, int l) {
+int hammingDistance(int *a , int *b) {
     int result = 0;
-    for(int i = 0; i < l; i++) {
-        if(a[i] != b[i])
-            result++;
+    for(int i = 0; i < WORD_MAX_SIZE; i++) {
+        int temp = a[i] ^ b[i];
+        result += __builtin_popcount(temp);
     }
     return result;
 }
 
 int main() {
     int n, l;
-    char** numbers;
     cin >> n >> l;
-    numbers = new char*[n];
+    int* mem = new int[WORD_MAX_SIZE * n];
     for(int i = 0; i < n; i++) {
-        numbers[i] = new char[l + 1];
+        for(int j = 0; j < WORD_MAX_SIZE; j++) {
+            mem[j + i*WORD_MAX_SIZE] = 0;
+        }
     }
-
     for(int i = 0; i < n; i++) {
-        cin >> numbers[i];
+        for(int j = 0; j < l; j++) {
+            char a;
+            do {
+                cin.get(a);
+            } while(isspace(a));
+            mem[j / 32 + i * WORD_MAX_SIZE] *= 2;
+            if(a == '1')
+                mem[j / 32 + i * WORD_MAX_SIZE]++;
+        }
     }
 
     for(int i = 0; i < n; i++) {
         for(int j = i + 1; j < n; j++) {
-            if(hammingDistance(numbers[i], numbers[j], l) == 1) {
+            if(hammingDistance(mem + i*WORD_MAX_SIZE, mem + j*WORD_MAX_SIZE) == 1) {
                 cout << i << " " << j << "\n";
             }
         }
     }
 
-    for(int i = 0; i < n; i++) {
-        delete[] numbers[i];
-    }
-    delete[] numbers;
+    delete[] mem;
     return 0;
 }
